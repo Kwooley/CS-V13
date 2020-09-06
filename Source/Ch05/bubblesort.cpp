a34a318153c145e309509c02133cdd622c784386
Kayw Lee <kwooler@gmail.com>
2020-09-06 00:59:02 -0700

    ch05

--- Source/Ch05/bubblesort.cpp
+++ Source/Ch05/bubblesort.cpp
@@ -17,7 +17,7 @@ int main()
 void bubblesort(int a[], int N)
 {
     for(int i=0;i<N-1;i++){
-        for (int j=0; j<N-1; j++)
+        for (int j=0; j<N-i; j++)
             if ( a[j] > a[j+1])
                 swap(a, j, j+1);
     }
